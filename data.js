83a67da67629f5047e736febd0536c60f7dcfa76
RobinTHOMAS <robin.19thomas@gmail.com*>
2021-11-30 17:29:13 +0100

    Fin

--- data.js
+++ data.js
@@ -1,28 +1,53 @@
 const content = document.getElementById("content")
+const contentP = document.getElementById("post")
 
+const data = fetch('https://jsonplaceholder.typicode.com/users/')
 
-    const data = fetch('https://jsonplaceholder.typicode.com/users/')
-        .then((response) => response.json())
-        .then((json) => {
-            console.log(data)
-            json.forEach((user, index) => {
-                let name = document.createElement('div');
-                name.innerHTML = `<p>${user.name}<p/>`
-                content.appendChild(name)
+    .then((response) => response.json())
+    .then((json) => {
+
+        json.forEach((user) => {
+            user1 = user.id;
+            let name = document.createElement('div');
+            name.innerHTML = `<p>${user.name}<p/>`
+            name.setAttribute('id', user.id);
+            content.appendChild(name)
+            name.addEventListener("click", async function(event){
+console.log(event)
+                const data1 = fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user1}`)
+                    
+                    .then((response) => response.json())
+                    .then((json) => {
+
+                        json.forEach((user) => {
+
+                            let title = document.createElement('div');
+                            title.innerHTML = `<p>Title: ${user.title} , Body: ${user.body}<p/>`
+                            contentP.appendChild(title)
+                        })
+                    })
             })
         })
+    })
+
+
+
+
 
 const valeur1 = document.getElementById("id");
 const OKButton = document.getElementById("OK");
 
 console.log(OKButton);
-function affiche(valeur1) {
 
-    fetch(`https://jsonplaceholder.typicode.com/users/${valeur1}`)
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-}
 
-OKButton.addEventListener('click', function () {
-    affiche(valeur1.value)
-})
+
+// function affiche(valeur1) {
+
+//     fetch(`https://jsonplaceholder.typicode.com/users/${valeur1}`)
+//          .then((response) => response.json())
+//          .then((json) => console.log(json));
+//  }
+
+//  OKButton.addEventListener('click', function () {
+//      affiche(valeur1.value)
+//  })
